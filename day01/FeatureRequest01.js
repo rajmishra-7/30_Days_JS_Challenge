e415438bc0913d2de0d6ded4e0ac3586afdd58b6
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-06 23:48:01 +0530

    feature request 1 completed

---
+++ day01/FeatureRequest01.js
@@ -0,0 +1,16 @@
+//Variables Type Console Log
+
+let num = 7;
+let str = "Finishers";
+let isLoggedIn = true;
+let person = {
+    name: "John Wick",
+    age: 30
+}
+let fruits = ['apple', 'mango', 'orange'];
+
+console.log(typeof num, num);
+console.log(typeof str, str);
+console.log(typeof isLoggedIn, isLoggedIn);
+console.log(typeof person, person);
+console.log(typeof fruits, fruits);
