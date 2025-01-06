edc18610ddbbe6aa8ffa04770fe462175fe8c41b
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-06 23:52:39 +0530

    feature request 2 completed

---
+++ day01/FeatureRequest02.js
@@ -0,0 +1,15 @@
+//Reassignment Demo
+
+let num1 = 25;
+
+const num2 = 25;
+
+console.log("Current value of num1 : ", num1);
+console.log("Current value of num2 : ", num2);
+
+num1 = 30;
+// num2 = 30; -> This will give error as we can't reassign const variables.
+
+console.log("After reassignment num1 : ", num1);
+console.log("After reassignment num2 : ", num2);
+
