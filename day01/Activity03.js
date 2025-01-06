dfa0dcad1e5ca11ea7a91aacc57e959332ad48d6
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-06 23:40:42 +0530

    Activity3 done

---
+++ day01/Activity03.js
@@ -0,0 +1,18 @@
+//DataTypes
+
+let num = 7;
+let str = "Finishers";
+let isLoggedIn = true;
+let person = {
+    name: "John Wick",
+    age: 30
+}
+let fruits = ['apple', 'mango', 'orange'];
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof isLoggedIn);
+console.log(typeof person);
+console.log(typeof fruits);
+
+
