3fdf80ec8f5b005770b96e838018c066b1e8bd40
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-07 23:52:04 +0530

    Activity 1 done

---
+++ day02/Activity01.js
@@ -0,0 +1,13 @@
+//Arithmetic Operators
+
+let num1 = 5, num2 = 2;
+
+console.log(`Sum : `, num1+num2);
+console.log(`Difference : `, num1-num2 );
+console.log(`Product : `, num1*num2);
+console.log('Quotient : ', num1/num2);
+console.log('Remainder : ', num1%num2);
+
+
+
+
