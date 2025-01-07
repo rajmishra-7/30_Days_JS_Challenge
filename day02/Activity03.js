d97d482ae1099c429e1db19c22fa93b088e39591
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-08 00:00:58 +0530

    Activity 3 done

---
+++ day02/Activity03.js
@@ -0,0 +1,20 @@
+//Comparison Operators
+
+let a = 5;
+let b = 2;
+
+console.log('a > b :', a > b);
+console.log('a < b :', a < b);
+console.log('a >= b :', a >= b);
+console.log('a <= b :', a <= b);
+
+let c = 20;
+let d = '20';
+
+console.log('c == d', c == d);
+console.log('c === d', c ===d);
+
+
+
+
+
