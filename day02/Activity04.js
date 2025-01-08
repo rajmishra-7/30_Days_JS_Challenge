8bbae13168888b6ce578109f4f3336a213d72f88
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-08 23:46:08 +0530

    Activity 4 done

---
+++ day02/Activity04.js
@@ -0,0 +1,9 @@
+// Logical Operators
+
+let a = 5, b = 3, c =2;
+
+if(a>b && a>c) console.log('a is largest');
+
+if(c<a || c<b) console.log('c is not largest');
+
+if(!false) console.log('!false = true');
