5bea2fba7fc0827fca2e5b15e8a552351f0152d9
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-08 23:52:32 +0530

    Feature request 1 done

---
+++ day02/FeatureRequest01.js
@@ -0,0 +1,11 @@
+// Arithmetic Operations Script
+
+let a = 5, b = 2;
+
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+let remainder = a % b;
+
+console.log(addition, subtraction, multiplication, division, remainder);
