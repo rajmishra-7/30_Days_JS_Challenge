df035803b5e42d0dcdfa981e8883648bf72ad971
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-09 00:00:26 +0530

    Activity 1 done

---
+++ day03/Activity01.js
@@ -0,0 +1,15 @@
+// if else statements
+
+let num = -3;
+
+if(num > 0) console.log('positive');
+else if(num < 0) console.log('negative');
+else console.log('zero');
+
+let personAge = 21;
+
+if(personAge >= 18) console.log('Eligible for voting');
+else console.log('Not Eligible for voting');
+
+
+
