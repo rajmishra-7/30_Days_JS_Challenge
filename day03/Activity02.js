1fecbbfaa232f2afadb7c1423e8386aca0290a73
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-09 22:13:25 +0530

    Activity 2 done

---
+++ day03/Activity02.js
@@ -0,0 +1,8 @@
+// Nested if else statements
+
+let c = 5, a = 2, b = 1;
+
+if (a > b ){
+    if(a > c) console.log('a is largest');
+    else console.log('c is largest');
+}else console.log('b is largest');
