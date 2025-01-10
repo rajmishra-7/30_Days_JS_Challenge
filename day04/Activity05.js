c2350be4ceda753f6b55b95822bc301a25eb7e5b
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-10 23:48:06 +0530

    Activity 5 done

---
+++ day04/Activity05.js
@@ -0,0 +1,8 @@
+// Loop control statements
+
+for (let index = 1; index <= 10; index++) {
+    if (index === 5)  continue;
+    else if (index === 7) break;
+    else console.log(index);
+    
+    }
