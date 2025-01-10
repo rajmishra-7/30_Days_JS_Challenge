9542fb50064552770a1f600f9b7f77a377cb3671
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-11 00:02:36 +0530

    day 5 activity 1 done

---
+++ day05/Activity01.js
@@ -0,0 +1,26 @@
+// Function Declaration
+
+function evenOrOdd(num) {
+    return num % 2 == 0 ? "Even" : "Odd";
+}
+
+console.log(evenOrOdd(7));
+console.log(evenOrOdd(9));
+console.log(evenOrOdd(19));
+console.log(evenOrOdd(2));
+
+
+
+
+
+function square(num) {
+    return num**2;
+}
+
+console.log(square(2));
+console.log(square(3));
+console.log(square(4));
+console.log(square(5));
+
+
+
