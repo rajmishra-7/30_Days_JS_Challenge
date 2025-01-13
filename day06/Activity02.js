aa6ea0de5faba0eeb580722986a97e36644fe7b1
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-13 23:50:40 +0530

    Activity 2 done

---
+++ day06/Activity02.js
@@ -0,0 +1,17 @@
+// Array methods basic
+
+let arr = [1,2,3,4,5];
+console.log('Original Array : ', arr);
+
+arr.push(7);
+console.log('push 7 : ', arr);
+
+arr.pop();
+console.log('pop :', arr);
+
+arr.unshift(0);
+console.log('unshift 0 : ', arr);
+
+arr.shift();
+console.log('shift : ', arr);
+
