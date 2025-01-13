5550774db9b437fd03965eae9195b73c7bd67f11
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-13 23:43:20 +0530

    Activity 1 done

---
+++ day06/Activity01.js
@@ -0,0 +1,13 @@
+// Array creation and access
+
+let arr = [1,2,3,4,5];
+
+console.log(arr);
+
+// access first element
+
+console.log(arr[0]);
+
+// access last element
+
+console.log(arr[arr.length-1]);
