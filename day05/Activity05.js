b48d5c287b703df3bbc89589074caa3c6c0d6b05
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-15 00:12:46 +0530

    Activity 5 done

--- day05/Activity05.js
+++ day05/Activity05.js
@@ -1,1 +1,15 @@
 // Higher order functions
+
+function highOrderFunctionDemo(n, func) {
+    for (let index = 0; index < n; index++) {
+        func(index);
+        
+    }
+}
+
+function func(i){
+    console.log("Hello! ",i);
+    
+}
+
+highOrderFunctionDemo(5, func);
