866c98567498063e87ad04377c044ea214b04e48
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-14 23:59:54 +0530

    Activity 4 done

---
+++ day06/Activity04.js
@@ -0,0 +1,16 @@
+// Array Iteration
+
+let arr = [10, 20, 30, 40, 50];
+
+// for loop
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element);
+    
+}
+
+// for-each loop
+arr.forEach(element => {
+    console.log(element);
+    
+});
