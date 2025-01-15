5529e18c171836343b00d31942173fc2d8fc6f1c
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-15 23:41:59 +0530

    Activity 1 done

---
+++ day07/Activity01.js
@@ -0,0 +1,13 @@
+// Object creation and Access
+
+const book = {
+    title : 'Harry Potter',
+    author : 'J.K. Rowling',
+    year : '2002'
+}
+
+console.log(book);
+
+console.log(book.title);
+console.log(book['author']);
+
