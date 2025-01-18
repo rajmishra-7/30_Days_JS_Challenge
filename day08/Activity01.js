e829b6d927dc2a75c40edc29a9d7e0c479f408c8
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-18 23:06:51 +0530

    Activity 1 done

---
+++ day08/Activity01.js
@@ -0,0 +1,20 @@
+// Template Literals
+
+//task1
+
+let username = 'John';
+let age = 26;
+
+let userDetails = `username is ${username} and age is ${age}`;
+
+console.log(userDetails);
+
+//task2
+
+let multilineString = `
+This is
+a multiline
+string example
+`;
+
+console.log(multilineString);
