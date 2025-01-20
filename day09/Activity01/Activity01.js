5f66f74cda83afe3445d90a554e375a224cb801e
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-21 00:24:21 +0530

    Activity 1 task 2 done

--- day09/Activity01/Activity01.js
+++ day09/Activity01/Activity01.js
@@ -1,3 +1,11 @@
 const heading = document.getElementById("heading");
 
 heading.textContent = "Hello Finishers!";
+
+const [divElement,] = document.getElementsByClassName("div");
+
+divElement.style.backgroundColor = "blue";
+divElement.style.color = "white";
+
+
+
