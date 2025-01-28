28c45e2968de7616f8b22362d7a7317891cc98a6
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-28 23:52:22 +0530

    Activity 5 done

--- day05/Activity05.js
+++ day05/Activity05.js
@@ -1,5 +1,6 @@
 // Higher order functions
 
+//task 1
 function highOrderFunctionDemo(n, func) {
     for (let index = 0; index < n; index++) {
         func(index);
@@ -13,3 +14,19 @@ function func(i){
 }
 
 highOrderFunctionDemo(5, func);
+
+//task 2
+
+function hof2(func1, func2, value) {
+    return func2(func1(value));
+}
+
+func1 = (value) => {
+    return ++value;
+}
+
+func2 = (value) => {
+    return value**2;
+}
+
+console.log(hof2(func1,func2,4));
