3bea7736aeabc891bc46918173d0a6a1e984350e
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-28 23:45:11 +0530

    Activity 5 done

--- day06/Activity05.js
+++ day06/Activity05.js
@@ -1,9 +1,14 @@
 // Multi-Dimensional Arrays
+
+// refer : https://www.freecodecamp.org/news/javascript-2d-arrays/
+
+
 let arr = [
     [10,20,30],
     [40,50,60],
     [70,80,90]
 ]
 console.log(arr);
-
+//task 2
 console.log(arr[2][0]);
+
