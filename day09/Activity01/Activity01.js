9f20b916d02cbcacc8e3dfb65c203f8da2aec2ed
Raj Mishra <rajmishra.sde@gmail.com>
2025-02-03 23:59:01 +0530

    comment added

--- day09/Activity01/Activity01.js
+++ day09/Activity01/Activity01.js
@@ -2,7 +2,7 @@ const heading = document.getElementById("heading");
 
 heading.textContent = "Hello Finishers!";
 
-const [divElement,] = document.getElementsByClassName("div");
+const [divElement,] = document.getElementsByClassName("div"); //only taking first element using destructuring.
 
 setTimeout(()=>{
     divElement.style.backgroundColor = "blue";
